e095f1ea2e2a5ba3e95fec1bca3f93747cfcac42
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-10-23 12:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const charMap = {};
+  for (let i = 0; i < str.length; i++) {
+    if (charMap[str[i]]) {
+      charMap[str[i]]++;
+    } else {
+      charMap[str[i]] = 1;
+    }
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (charMap[str[i]] === 1) {
+      return str[i];
+    }
+  }
+  return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+const result = firstNonRepeatedChar(input);
+if (result) {
+  alert(`The first non-repeated character is: ${result}`);
+} else {
+  alert("There are no non-repeated characters in the string.");
+}
